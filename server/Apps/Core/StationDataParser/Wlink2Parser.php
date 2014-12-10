22ed908e528ddd31c610313206e797754193b3d0
sardegnaclima <info@sardegna-clima.it>
2014-12-10 21:39:16 +0100

    Update Wlink2Parser.php

--- server/Apps/Core/StationDataParser/Wlink2Parser.php
+++ server/Apps/Core/StationDataParser/Wlink2Parser.php
@@ -1,7 +1,7 @@
 <?php
 namespace StationDataParser;
 require_once __DIR__ ."/../../../vendor/autoload.php";
-class WundergroundParser extends Parser implements StationParserInterface{
+class Wlink2Parser extends Parser implements StationParserInterface{
 	public function getMeasure($data_url){
 		$dataraw = simplexml_load_file($data_url);
         if(!$dataraw)return null;
@@ -11,27 +11,25 @@ class WundergroundParser extends Parser implements StationParserInterface{
 		$datetime = date("Y-m-d H:i:s", strtotime($array_data));
 		$array_datetime = explode(" ", $datetime);
 		$lineday = $array_datetime[0] . ' 00:00:00';
-		\Logger::getLogger('measure_snapshot')->debug("WUNDERGROUND DEBUG");
-		\Logger::getLogger('measure_snapshot')->debug("line date DEBUG: ");
-		\Logger::getLogger('measure_snapshot')->debug($lineday);
 		$array_temp = $dataraw->temp_c; 
+		$array_min = $dataraw->davis_current_observation->temp_day_low_f; 
+		$array_max = $dataraw->davis_current_observation->temp_in_day_high_f; 
 		$array_hum = $dataraw->relative_humidity; 
 		$array_dp = $dataraw->dewpoint_c;
 		$array_wind = $dataraw->wind_mph;
+		$array_windchill = $dataraw->windchill_c;
+		$array_heatindex = $dataraw->heat_index_c;
 		$array_dir = $dataraw->wind_dir;
-		$array_rdy = $dataraw->precip_today_in;
+		$array_rdy = $dataraw->davis_current_observation->rain_day_in;
+		$array_rmt = $dataraw->davis_current_observation->rain_month_in;
+		$array_ryr = $dataraw->davis_current_observation->rain_year_in;
 		$array_bar = $dataraw->pressure_mb;
-		$wspeed =  floatval($array_wind)*1.609;
-		$wspeed =  number_format($wspeed, 1, '.', '');
-		$rain = floatval($array_rdy)*25.4;
-		$rain = number_format($rain, 1, '.', '');
+
 		// -- create measure -- //
 		$measure = new \Measure();
 		$measure->setTemp(floatval($array_temp));
-		$maxInDB = $this->em->getRepository('Measure')->getTempMaxByStation($this->station,$lineday);
-		$measure->setTempmax(($maxInDB===null)?$temp:max(array($maxInDB, floatval($array_temp))));
-		$minInDB = $this->em->getRepository('Measure')->getTempMinByStation($this->station,$lineday);
-		$measure->setTempmin(($minInDB===null)?$temp:min(array($minInDB, floatval($array_temp))));
+		$measure->setTempmax(floatval($array_max));
+		$measure->setTempmin((floatval($array_min)););
 		$measure->setHum(floatval($array_hum));
 		$measure->setDp(floatval($array_dp));
 		$measure->setWchill(null);
@@ -41,8 +39,8 @@ class WundergroundParser extends Parser implements StationParserInterface{
 		$measure->setBar(floatval($array_bar));
 		$measure->setRain($rain);
 		$measure->setRr(null);
-		$measure->setRainmt(null);
-		$measure->setRainyr(null);
+		$measure->setRainmt(floatval($array_rmt));
+		$measure->setRainyr(floatval($array_ryr));
 		$dateObj = date_create($datetime);
 		$measure->setDate($dateObj?$dateObj:null);
 			
